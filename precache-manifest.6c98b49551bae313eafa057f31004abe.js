d3fb176e41e319639f0cc1ce8fbc7c3541a4a6d7
charlie <charliewynn@gmail.com>
2018-11-30 22:40:13 -0600

    Updates

---
+++ precache-manifest.6c98b49551bae313eafa057f31004abe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pokemon-wish-list/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1773593dcda69de5f05f",
+    "url": "/pokemon-wish-list/static/js/runtime~main.1773593d.js"
+  },
+  {
+    "revision": "40bfa90c26ba0e8b4194",
+    "url": "/pokemon-wish-list/static/js/main.40bfa90c.chunk.js"
+  },
+  {
+    "revision": "4997f263500b67389ba1",
+    "url": "/pokemon-wish-list/static/js/1.4997f263.chunk.js"
+  },
+  {
+    "revision": "40bfa90c26ba0e8b4194",
+    "url": "/pokemon-wish-list/static/css/main.10647d74.chunk.css"
+  },
+  {
+    "revision": "5eaf82ba19279aa321335db288989faf",
+    "url": "/pokemon-wish-list/index.html"
+  }
+];
